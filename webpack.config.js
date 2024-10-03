125199f6112d46e917ab9262b18e7bfbd24026e9
Tran McFarland Nguyen <73681436+Tranimal-N@users.noreply.github.com>
2024-10-03 16:27:53 -0700

    boilerplate code to fix

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Development",
+            template: path.resolve(__dirname, "./src/index.html"),
+        }),
+    ],
+  module:{
+    rules:[
+      {
+        test: /\.m?(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+    ]
+  },
+  devServer: {
+    port: 8080,
+    static: {
+        directory: path.join(__dirname)
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+      secure: false,
+    },
+  }
+}
